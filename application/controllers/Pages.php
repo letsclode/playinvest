8d14ce61dc53826b17178a571c2e94df15c5ba24
Clodeday <cloderaldo@gmail.com>
2019-08-14 18:10:50 +0800

    -deleted post not provided

--- application/controllers/Pages.php
+++ application/controllers/Pages.php
@@ -6,7 +6,6 @@
             }
             $data['title'] = ucfirst($page);
             $this->load->view('templates/header');
-            $this->load->view('templates/header');
             $this->load->view('pages/'.$page, $data);
             $this->load->view('templates/footer');
         }
